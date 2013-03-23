4219b60ba08490f8117234f780e5a7075fbde295
sylviapsh <sylviapsh@gmail.com>
2013-03-23 18:17:14 +0200

    Create Company.cs

---
+++ Bank/Company.cs
@@ -0,0 +1,9 @@
+namespace Bank
+{
+    public class Company : Customer
+    {
+        public Company(int companyID, string name, string address, string phone) : base(companyID, name, address, phone) { }
+        
+    }
+
+}
