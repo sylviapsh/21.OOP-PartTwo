3292b0effb65274cc547ee2d0cb2c4ed4673c51a
sylviapsh <sylviapsh@gmail.com>
2013-03-23 18:18:49 +0200

    Create Individual.cs

---
+++ Bank/Individual.cs
@@ -0,0 +1,7 @@
+namespace Bank
+{
+    public class Individual : Customer
+    {
+        public Individual(int individualID, string name, string address, string phone) : base(individualID, name, address, phone) { }
+    }
+}
